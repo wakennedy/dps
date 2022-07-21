0267d24c553993997c4513841e82c1bdf30dc25a
wakennedy <alexkenned@gmail.com>
2022-07-21 15:37:50 -0700

    gitignore

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import { prisma } from "../lib/prisma";
 
 interface Discs {
   discs: {
+    id: string;
     brand: string;
     name: string;
     plastic: string;
@@ -109,6 +110,7 @@ const Home = ({ discs }: Discs) => {
       <form
         onSubmit={(e) => {
           e.preventDefault();
+          handleSubmit(form);
         }}
       >
         <input
@@ -164,8 +166,52 @@ const Home = ({ discs }: Discs) => {
           Add +
         </button>
       </form>
+      <br />
+
+      <div className="w-auto min-w-[25%] max-w-min mt-20 mx-auto space-y-6 flex flex-col items-stretch">
+        <ul>
+          {discs.map((disc) => (
+            <li key={disc.id} className="border-b border-gray-600 p-2">
+              <div className="flex justify-between">
+                <div className="flex-1">
+                  <h3 className="font-bold">{disc.brand}</h3>
+                  <p className="text-sm">{disc.plastic}</p>
+                  <p className="text-sm">{disc.name}</p>
+                  <p className="text-sm">{disc.speed}</p>
+                  <p className="text-sm">{disc.glide}</p>
+                  <p className="text-sm">{disc.turn}</p>
+                  <p className="text-sm">{disc.fade}</p>
+                </div>
+                {/* <button onClick={() => setForm({title: note.title, content: note.content, id: note.id})} className="bg-blue-500 mr-3 px-3 text-white rounded">Update</button>
+                <button onClick={() => deleteNote(note.id)} className="bg-red-500 px-3 text-white rounded">X</button> */}
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
 
 export default Home;
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const discs = await prisma.discs.findMany({
+    select: {
+      brand: true,
+      id: true,
+      name: true,
+      plastic: true,
+      speed: true,
+      glide: true,
+      turn: true,
+      fade: true,
+    },
+  });
+
+  return {
+    props: {
+      discs,
+    },
+  };
+};
