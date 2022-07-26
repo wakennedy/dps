1c4cd96e746beb7535439720aa719985d269ce78
wakennedy <alexkenned@gmail.com>
2022-07-26 15:56:23 -0700

    fixed a bit of import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Toaster } from "react-hot-toast";
 import Navbar from "../components/Navbar";
+import React from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
